f8eefcdefd2da10264a535bd908de4c386d3b13e
Tiago Dias <tiagodiaslaureano32@gmail.com>
2021-02-19 11:25:16 -0300

    Adição do csv_create.py

---
+++ src/csv_create.py
@@ -0,0 +1,15 @@
+import csv
+
+def create_csv(arg1):
+    global arq
+    nome_arquivo = arg1.replace('.','_')
+    arq = open(f'{nome_arquivo}.csv', 'w')
+    esc = csv.writer(arq)
+    esc.writerow(('IP', 'PORTA', 'STATUS', 'SERVIÇO'))
+
+def make_csv(arg1, arg2, arg3, arg4):
+    esc = csv.writer(arq)
+    esc.writerow((arg1, arg2, arg3, arg4))
+
+def close_csv():
+    arq.close()
