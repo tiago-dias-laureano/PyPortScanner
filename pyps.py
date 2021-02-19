b94b32a0aacee730c3eebb3260b602ecf896adb0
Tiago Dias <tiagodiaslaureano32@gmail.com>
2021-02-19 11:24:39 -0300

    Adicionado funções de criação de arquivo CSV.
    Adicionado funções de criação de arquivo CSV para geração de relátorios.

--- pyps.py
+++ pyps.py
@@ -3,6 +3,7 @@ import socket
 import datetime
 from src import code_info
 from src import main_ports
+from src import csv_create
 
 parser = argparse.ArgumentParser(description='PyPortScanner v1.0')
 
@@ -22,6 +23,7 @@ def scanner_ip(args):
     verbose = "false"
     ip = args.ip
     verbose = bool(args.verbose)
+    csv_create.create_csv(ip)
     print(f'[*] {datetime.datetime.now().strftime("%H:%M:%S")} [*] #> Scanner Iniciado No IP: {ip}')
     for porta in main_ports.return_port():
         conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,18 +32,23 @@ def scanner_ip(args):
 
         if resposta == 0:
             print(f'[*] {datetime.datetime.now().strftime("%H:%M:%S")} [*] #> A porta {porta} ({code_info.dict_de_code[str(porta)]}) está ABERTA!')
-        elif resposta == 111 or resposta == 13:    
-            print(f'[!] {datetime.datetime.now().strftime("%H:%M:%S")} [!] #> A conexão com a porta {porta} ({code_info.dict_de_code[str(porta)]}) foi RECUSADA / NEGADA!')
+            csv_create.make_csv(ip, porta, 'ABERTA', code_info.dict_de_code[str(porta)])
+        elif resposta == 111 or resposta == 13:   
+            if verbose:
+                print(f'[!] {datetime.datetime.now().strftime("%H:%M:%S")} [!] #> A conexão com a porta {porta} ({code_info.dict_de_code[str(porta)]}) foi RECUSADA / NEGADA!')
+            csv_create.make_csv(ip, porta, 'RECUSADA / NEGADA', code_info.dict_de_code[str(porta)])
         else:
-            if verbose.upper() == "TRUE":
+            csv_create.make_csv(ip, porta, 'FECHADA', code_info.dict_de_code[str(porta)])
+            if verbose:
                 print(f'[*] {datetime.datetime.now().strftime("%H:%M:%S")} [*] #> A porta {porta} ({code_info.dict_de_code[str(porta)]}) está FECHADA!')
             else:
                 pass
 
 def scanner_url(args):
+
     url = args.url
     verbose = bool(args.verbose)
-
+    csv_create.create_csv(url)
     if 'http://' in args.url:
         url_parser = args.url.split('http://')
         url_parser = url_parser[1]
@@ -57,10 +64,14 @@ def scanner_url(args):
         resposta = conn.connect_ex((ip, porta))
 
         if resposta == 0:
+            csv_create.make_csv(url, porta, 'ABERTA', code_info.dict_de_code[str(porta)])
             print(f'[*] {datetime.datetime.now().strftime("%H:%M:%S")} [*] #> A porta {porta} ({code_info.dict_de_code[str(porta)]}) está ABERTA!')
         elif resposta == 111 or resposta == 13:    
-            print(f'[!] {datetime.datetime.now().strftime("%H:%M:%S")} [!] #> A conexão com a porta {porta} ({code_info.dict_de_code[str(porta)]}) foi RECUSADA / NEGADA!')
+            csv_create.make_csv(url, porta, 'RECUSADA / NEGADA', code_info.dict_de_code[str(porta)])
+            if verbose:
+                print(f'[!] {datetime.datetime.now().strftime("%H:%M:%S")} [!] #> A conexão com a porta {porta} ({code_info.dict_de_code[str(porta)]}) foi RECUSADA / NEGADA!')
         else:
+            csv_create.make_csv(ip, porta, 'FECHADA', code_info.dict_de_code[str(porta)])
             if verbose:
                 print(f'[*] {datetime.datetime.now().strftime("%H:%M:%S")} [*] #> A porta {porta} ({code_info.dict_de_code[str(porta)]}) está FECHADA!')
             else:
