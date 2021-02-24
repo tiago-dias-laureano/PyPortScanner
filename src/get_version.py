0c5a70d7fab26dad43f7f0c113d040c22d3f8b6e
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-02-24 16:19:09 -0300

    Improve argument names

--- src/get_version.py
+++ src/get_version.py
@@ -1,34 +1,34 @@
 import socket  
 
-def get_version(arg1, arg2):
+def get_version(ip_address, port):
     try:
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((arg1,arg2)) 
+        s.connect((ip_address, port)) 
         s.settimeout(2)
         byte = str.encode("Server:\r\n")
         s.send(byte)
 
         banner = s.recv(1024)
         if str(banner):
-            if arg2 == 21:
+            if port == 21:
                 if 'Pure-FTPd' in str(banner):
                     return 'Pure-FTPd'
                 elif 'FTP Server' in str(banner):
                     return 'FTP Server'
             
-            if arg2 == 22:
+            if port == 22:
                 if 'OpenSSH' in str(banner):
                     return 'OpenSSH'
 
-            if arg2 == 25:
+            if port == 25:
                 if 'ESMTP Exim' in str(banner):
                     return 'ESMTP Exim'
             
-            if arg2 == 26:
+            if port == 26:
                 if 'ESMTP Exim' in str(banner):
                     return 'ESMTP Exim'
             
-            if arg2 == 80:
+            if port == 80:
                 if 'Apache' in str(banner):
                     return 'Apache'
                 elif 'nginx' in str(banner):
@@ -42,15 +42,15 @@ def get_version(arg1, arg2):
                 elif 'cloudflare' in str(banner):
                     return 'Cloudflare'
 
-            if arg2 == 110:
+            if port == 110:
                 if 'Dovecot' in str(banner):
                     return 'Dovecot pop3d'
 
-            if arg2 == 143:
+            if port == 143:
                 if 'IMAP' in str(banner):
                     return 'Dovecot imapd'
             
-            if arg2 == 443:
+            if port == 443:
                 if 'nginx' in str(banner):
                     return 'Nginx'
                 elif 'aruba-proxy' in str(banner):
@@ -60,10 +60,10 @@ def get_version(arg1, arg2):
                 elif 'cloudflare' in str(banner):
                     return 'Cloudflare'
 
-            if arg2 == 3306:
+            if port == 3306:
                 return 'MySql'
 
-            if arg2 == 8080:
+            if port == 8080:
                 if 'nginx' in str(banner):
                     return 'Nginx'
                 elif 'aruba-proxy' in str(banner):
