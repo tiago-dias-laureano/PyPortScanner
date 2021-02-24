e22a8cfa1a978d4ffa91d5cd7de9c33c636c3b5e
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-02-24 15:51:05 -0300

    Update to new structure

--- pyps.py
+++ pyps.py
@@ -1,24 +1,34 @@
+# -*- coding: utf-8 -*-
+
 import argparse
-from src import scanner_ip
-from src import scanner_url
 
-parser = argparse.ArgumentParser(description='PyPortScanner v1.0')
+from src import scanner
 
-parser.add_argument('--url', 
-                    required=False, 
-                    help='Set a url que deseja fazer o scanner.')
-parser.add_argument('--ip', 
-                    required=False, 
-                    help="Set o ip que deseja fazer o scanner." )
-parser.add_argument('--verbose',
-                    required=False, 
-                    help="Com essa opção o scanner retorna todos os detalhes. Set o valor True para obter todos os detalhes",
-                    )
-args = parser.parse_args()
 
-if args.url:
-    scanner_url.scanner_url(args)
-elif args.ip:
-    scanner_ip.scanner_ip(args)
-else:
-    print('Você precisa setar um argumento correto!')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyPortScanner v1.0')
+
+    parser.add_argument('--url', 
+                        required=False, 
+                        help='Set a url que deseja fazer o scanner.')
+
+    parser.add_argument('--ip', 
+                        required=False, 
+                        help="Set o ip que deseja fazer o scanner." )
+                        
+    parser.add_argument('-v', '--verbose',
+                        required=False,
+                        default=False,
+                        help="Com essa opção o scanner retorna todos os detalhes. Set o valor True para obter todos os detalhes",
+                        )
+
+    args = parser.parse_args()
+
+    if args.url:
+        scanner.url(args.url, args.verbose)
+
+    elif args.ip:
+        scanner.ip(args.ip, args.verbose)
+        
+    else:
+        print('Você precisa setar um argumento correto!')
